154bda31e75c5d6ce50673847c4dfc0c6754f7a4
Patrick <cryne@gmx.de>
2021-12-01 16:08:39 +0100

    Day 01

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -1,7 +1,23 @@
 pub(crate) fn a(input: &str) -> i64 {
-    todo!();
+    let lines = input.lines().map(|line| line.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+
+    let mut increase_counter = 0;
+    for index in 1..lines.len() {
+        if lines[index] > lines[index-1] {
+            increase_counter += 1;
+        }
+    }
+    return increase_counter;
 }
 pub(crate) fn b(input: &str) -> i64 {
-    todo!();
+    let lines = input.lines().map(|line| line.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+
+    let mut increase_counter = 0;
+    for index in 3..lines.len() {
+        if lines[index] > lines[index-3] {
+            increase_counter += 1;
+        }
+    }
+    return increase_counter;
 }
 
