a0eaf79b5786cbaa046860df9e73874cb5a013e5
Patrick <cryne@gmx.de>
2021-12-02 20:15:58 +0100

    Put session key in seperate file

--- src/days/util.rs
+++ src/days/util.rs
@@ -1,5 +1,6 @@
+use std::fs::read_to_string;
 use reqwest::header::{COOKIE, HeaderMap};
-use reqwest::Method;
+use reqwest::{Method, StatusCode};
 
 pub(crate) async fn get_puzzle_input(day: i8) -> String {
     if let Ok(input) = std::fs::read_to_string(format!("inputs/day_{:02}.txt", day)) {
@@ -7,13 +8,18 @@ pub(crate) async fn get_puzzle_input(day: i8) -> String {
     }
 
     let mut headers = HeaderMap::new();
-    todo!("Add your own session key here");
-    headers.insert(COOKIE, "session=SESSION_KEY_HERE".parse().unwrap());
+
+    let session_key = read_to_string("session_key.txt")
+        .expect("Failed to read \"session_key.txt\". Did you create the file (at the right place)?");
+    headers.insert(COOKIE, format!("session={}", session_key).parse().unwrap());
 
     let client = reqwest::Client::new();
     let request = client.request(Method::GET, format!("https://adventofcode.com/2021/day/{}/input", day)).headers(headers).build().unwrap();
 
     let res = client.execute(request).await.unwrap();
+    if res.status() != StatusCode::OK {
+        panic!("There was an error while fetching the input data: {}. Did you input your session key correctly?", res.status())
+    }
     let input = res.text().await.unwrap();
 
     if !std::path::Path::new("inputs").exists() {
